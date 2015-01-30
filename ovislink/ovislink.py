169a6e5f2a0e0698adb0764de9c7eeb0e83dae82
Nicolas <captainblood13007@gmail.com>
2015-01-30 11:52:51 -0800

    Update ovislink.py

--- ovislink/ovislink.py
+++ ovislink/ovislink.py
@@ -1,6 +1,6 @@
 #  Copyright (c) 2013, Nicolas Tarral
 #
-#  motorola_guy.py is free software; you can redistribute it and/or modify
+#  Ovislink.py is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
